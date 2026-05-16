b83b9eb91d0162af7e8d61bfac9dc0ca9e940719
dev-Aatif <Aatiff.P@proton.me>
2026-05-16 18:36:06 +0500

    Restore missing TITLES_FILE variable

--- bot.py
+++ bot.py
@@ -22,6 +22,7 @@ GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 PINS_DIR = os.path.join(BASE_DIR, "data", "pins")
 DONE_DIR = os.path.join(BASE_DIR, "data", "done")
+TITLES_FILE = os.path.join(BASE_DIR, "data", "titles.txt")
 RECENT_FILE = os.path.join(BASE_DIR, "data", "recent.json")
 
 # Supported image types
